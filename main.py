566a99b4c7f4feced9f0b0c2642d44cb82674596
kaiser-data <61822885+kaiser-data@users.noreply.github.com>
2025-01-11 23:45:31 +0100

    added list_all_product function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import products
-import store
+from products import Product
+from store import Store
 
 def start():
     menu = """
@@ -12,15 +12,29 @@ def start():
     """
     print(menu)
 
+def list_all_products(best_buy):
+
+    [product.show() for product in best_buy.get_all_products()]
+
+
+
+
+def menu_logic (user_input):
+    pass
+
+
 def main():
     # setup initial stock of inventory
-    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                    products.Product("Google Pixel 7", price=500, quantity=250)
+    product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                    Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    Product("Google Pixel 7", price=500, quantity=250)
                     ]
-    best_buy = store.Store(product_list)
+    best_buy = Store(product_list)
 
     start()
+    list_all_products(best_buy)
+
+
 
 if __name__ == '__main__':
     main()
